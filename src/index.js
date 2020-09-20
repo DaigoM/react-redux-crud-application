0ca25fe265b3780e7e70ba47cbbbc0f881da6986
Diego Mizutani <diego.mizu7@gmail.com>
2020-09-20 15:23:20 +0900

    learn store and provider from redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// store作成パッケージ
+import { createStore } from 'redux'
+// storeを全コンポーネントに渡す機能を持つ
+import { Provider } from 'react-redux'
+
 import './index.css';
-import App from './App';
+import reducer from './reducers'
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
+// store生成
+// 唯一のstore
+// アプリケーション内部の全stateはこのstoreに集約されている
+const store = createStore(reducer)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  </Provider>
 );
 
 // If you want your app to work offline and load faster, you can change
